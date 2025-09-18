cb1c6e788dc1317a17f14c736758b103a86fbadb
Josh Chernetsky <joshcherndev@gmail.com>
2025-09-18 15:24:09 -0700

    Inventory slots and draggable items added

---
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventorySlot : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (transform.childCount != 0) return;
+        GameObject dropped = eventData.pointerDrag;
+        DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
+        draggableItem.parentAfterDrag = transform;
+    }
+}
