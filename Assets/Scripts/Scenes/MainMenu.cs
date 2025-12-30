09aaf7e50dde46200b9195193e97ed6e47d842ab
Josh Chernetsky <joshcherndev@gmail.com>
2025-12-29 18:29:07 -0800

    main menu skeleton added to go into first level

--- Assets/Scripts/Scenes/MainMenu.cs
+++ Assets/Scripts/Scenes/MainMenu.cs
@@ -1,11 +1,37 @@
+using UnityEditor;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private SceneField sceneToLoad;
+    
+    [Header("UI Elements")]
+    [SerializeField] private GameObject SettingsScreen;
+
+    private void Start()
+    {
+        SettingsScreen.SetActive(false);
+    }
 
     public void StartButtonPressed()
     {
         SceneSwapManager.SwapScene(sceneToLoad);
+    }
+
+    public void SettingsButtonPressed()
+    {
+        SettingsScreen.SetActive(true);
+    }
+
+    public void QuitButtonPressed()
+    {
+        // TODO: change to use this when creating a build
+        // Application.Quit();
+        EditorApplication.ExitPlaymode();
+    }
+
+    public void CloseSettings()
+    {
+        SettingsScreen.SetActive(false);
     }
 }
