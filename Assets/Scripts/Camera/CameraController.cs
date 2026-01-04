9db568e748f68cd49d59492f80611991c004d1c8
Josh Chernetsky <joshcherndev@gmail.com>
2026-01-03 22:52:52 -0800

    camera handles collisions
    - Uses Phsyics.SphereCast to find collisions

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,3 +1,5 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,14 +9,17 @@ public class CameraController : MonoBehaviour
     [SerializeField] private InputActionAsset actions;
     [SerializeField] private Transform cameraTarget;
     [SerializeField] private GameObject playerMoveOrientation;
+    [SerializeField] private TextMeshProUGUI cameraLocalPosition;
 
     [Header("Settings")] 
     [SerializeField] private float maxXRotation = 70f;
     [SerializeField] private float minXRotation = -45f;
     [SerializeField] private float maxOrbitDistance = 3f;
     [SerializeField] private float minOrbitDistance = 0.1f;
-    [SerializeField] private float xSensitivity = 5f;
-    [SerializeField] private float ySensitivity = 5f;
+    [SerializeField] private float xSensitivity = 1f;
+    [SerializeField] private float ySensitivity = 1f;
+    [Range(0.01f, 2.0f), SerializeField] private float cameraRadius = 0.25f;
+    [Range(0.01f, 0.5f), SerializeField] private float adjustAmount = 0.25f;
     
     private Camera _cam;
     
@@ -30,6 +35,8 @@ public class CameraController : MonoBehaviour
     private bool _isAiming;
     private bool _playerDead;
     
+    private LayerMask _mask;
+    
     private void Start()
     {
         _cam = GetComponent<Camera>();
@@ -43,6 +50,9 @@ public class CameraController : MonoBehaviour
         
         transform.position = cameraTarget.transform.position - cameraTarget.transform.forward * maxOrbitDistance;
         Cursor.lockState = CursorLockMode.Locked;
+        
+        // Assign layers that the gun collides with
+        _mask = LayerMask.GetMask("Environment");
     }
 
     private void LateUpdate()
@@ -53,6 +63,8 @@ public class CameraController : MonoBehaviour
         GetMouseInput();
         RotateCameraAroundPoint();
         RotatePlayerMoveOrientation();
+
+        cameraLocalPosition.text = "Local Pos: (" + Math.Round(transform.localPosition.x, 2) + ", " + Math.Round(transform.localPosition.y, 2) + ", " + Math.Round(transform.localPosition.z, 2) + ")";
     }
 
     public void OnAim(InputAction.CallbackContext context)
@@ -64,19 +76,37 @@ public class CameraController : MonoBehaviour
     {
         _mouseInput.x = Input.GetAxis("Mouse X");
         _mouseInput.y = Input.GetAxis("Mouse Y");
-        _mouseInput.Normalize();
     }
 
     private void RotateCameraAroundPoint()
     {
-        xRotation -= Input.GetAxis("Mouse Y");
-        yRotation += Input.GetAxis("Mouse X");
-        
+        // update rotation with mouse input
+        xRotation -= _mouseInput.y * xSensitivity;
+        yRotation += _mouseInput.x * ySensitivity;
+
         float xRotationClamped = Mathf.Clamp(xRotation, minXRotation, maxXRotation);
         Quaternion targetRotation = Quaternion.Euler(xRotationClamped, yRotation, 0f);
-
         Vector3 offset = new Vector3(0f, 0f, maxOrbitDistance);
-        transform.position = cameraTarget.position - targetRotation * offset;
+        Vector3 targetPosition = cameraTarget.position - targetRotation * offset;
+        
+        // mouse input caused collision, adjust transform/rotation
+        if (Physics.SphereCast(
+                cameraTarget.position, cameraRadius, targetPosition.normalized, 
+                out RaycastHit hit, maxOrbitDistance, _mask))
+        {
+            float distance = Vector3.Distance(cameraTarget.position, hit.point);
+            distance -= 0.2f; // subtract how much the camera is offset from hit point
+
+            //if (Math.Abs(distance) < 0.2f)
+            //{
+            //    distance -= 0.2f;
+            //}
+
+            offset.z = distance;
+            targetPosition = cameraTarget.position - targetRotation * offset;
+        }
+        
+        transform.position = targetPosition;
         transform.rotation = targetRotation;
     }
 
